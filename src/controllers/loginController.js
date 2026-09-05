b68e903a1958ba218fc0a91486fc9d19a18f76e5
Marcelo <marceloamaral1364@gmail.com>
2026-09-05 15:24:39 -0300

    feat: Adding register controller

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,3 +2,9 @@ exports.index = (req, res) => {
     res.render('login');
     return;
 }
+
+exports.register = (req, res) => {
+    res.send('TESTETESTE');
+    console.log("ESTOU NO REGISTER");
+    return;
+}
